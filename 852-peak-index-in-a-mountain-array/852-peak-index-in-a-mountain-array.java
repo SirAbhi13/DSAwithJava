6059a3db627e8426a178a15ebaebf6ce38ef112c
Abhinav Yadav <72503824+SirAbhi13@users.noreply.github.com>
2022-07-31 02:29:02 +0530

    Time: 0 ms (100.00%), Space: 73.5 MB (10.41%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.java
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.java
@@ -5,26 +5,14 @@ class Solution {
         
         while(l<r){
             int mid = (r-l)/2 + l;
-            //Making sure we don't get out of bound error
-            if(mid!=0)
-            {   //if the value is the peak (checking left and right)
-                if (arr[mid] > arr[mid + 1] && arr[mid] > arr[mid - 1])
-                    return mid;
-                //if the value of mid is in increasing part of the array
-                else if (arr[mid] < arr[mid + 1] && arr[mid] > arr[mid - 1]) {
-                    l = mid + 1;
-                } 
-                // if the value of mid in decreasing part of the array
-                else if (arr[mid] > arr[mid + 1] && arr[mid] < arr[mid - 1]) {
-                    r = mid - 1;
-                }
+            if(arr[mid]>arr[mid+1]){
+                r = mid;
             }
             else{
-                if(arr[mid]>arr[1])
-                    return mid;
-                else
-                    return 1;
+                l = mid+1;
             }
+            
+            
         }
         return l;
     }
