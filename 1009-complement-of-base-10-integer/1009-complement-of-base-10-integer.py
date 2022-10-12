5773001b0d40144fc16050fccee332ac5c62172f
Abhinav Yadav <72503824+SirAbhi13@users.noreply.github.com>
2022-10-12 07:30:35 +0530

    Time: 53 ms (51.07%), Space: 13.8 MB (54.43%) - LeetHub

--- 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.py
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.py
@@ -1,26 +1,13 @@
 class Solution:
     def bitwiseComplement(self, n: int) -> int:
+        if n == 0: return 1
+        copy = n
+        mask = 0
         
-        if n ==0 : return 1
-        l = self.decToBin(n)        
-        for i in range(len(l)):
-            if l[i] == 1:
-                l[i] = 0
+        while copy:
+            mask = (mask << 1) | 1
+            copy >>=1
             
-            elif l[i] == 0:
-                l[i] = 1
-        ans =0
-        for i in range(len(l)-1,-1,-1):
-            ans = ans + l[i]*(2**i)
+        ans = ~n & mask
+        
         return ans
-                
-    
-    
-    def decToBin(self,num):
-        ans = []
-        i = 0
-        while num !=0:
-            rem = num%2
-            num >>= 1
-            ans.append(rem)
-        return (ans)
